618e1e36a506c06999f7ee054ed5f3102ea4fb77
Tommy Brosman <tommy.brosman@gmail.com>
2019-05-27 20:41:21 -0700

    Tweaked sample and split apart build file.

--- samples/01-PostGetTest/src/ClientMain.hx
+++ samples/01-PostGetTest/src/ClientMain.hx
@@ -5,10 +5,11 @@ import Sys;
 
 class ClientMain
 {
-	static function testSync()
-	{
-		trace("Testing sync");
-	    var inParams = new Map<String, String>();
+#if !flash
+    static function testSync()
+    {
+        trace("Testing sync");
+        var inParams = new Map<String, String>();
         inParams.set("key", "test");
         inParams.set("value", "asdf");
         RestClient.post(
@@ -22,39 +23,50 @@ class ClientMain
             outParams);
             
         trace("Returned from server: " + result);
-	}
-	
-	static function testAsync()
-	{
-		trace("Testing async");
-		var inParams = new Map<String, String>();
+    }
+#end
+    
+    static function testAsync()
+    {
+        trace("Testing async");
+        var inParams = new Map<String, String>();
         inParams.set("key", "test");
         inParams.set("value", "asdf");
         RestClient.postAsync(
             "http://localhost:8000",
-			function(result)
-			{
-				trace("POST returned");
-			},
-            inParams);
+            function(result)
+            {
+                trace("POST returned");
+            },
+            inParams,
+            function(error)
+            {
+                trace('Error from POST: ${error}');
+            });
         
         var outParams = new Map<String, String>();
         outParams.set("key", "test");
         RestClient.getAsync(
             "http://localhost:8000",
-			function(result)
-			{
-				trace("Returned from server: " + result);
-			},
-            outParams);
-	}
-	
+            function(result)
+            {
+                trace("Returned from server: " + result);
+            },
+            outParams,
+            function(error)
+            {
+                trace('Error from GET: ${error}');
+            });
+    }
+    
     static function main()
     {
-		testSync();
-		testAsync();
+#if !flash
+        testSync();
+#end
+        testAsync();
 #if sys
-		trace("Press Enter to close the client...");
+        trace("Press Enter to close the client...");
         Sys.stdin().readLine();
 #end
     }
